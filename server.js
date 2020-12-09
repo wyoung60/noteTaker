58da55c0abfb06247fd0f0cfd9bf655124c95760
Wes Young <wesley.young@du.edu>
2020-12-08 22:17:29 -0700

    Added heroku PORT and emptied db.json file

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const shortid = require("shortid");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 //Review these two lines of code.
 app.use(express.urlencoded({ extended: true }));
