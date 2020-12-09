11e3cec260f0c6bb488bab775937d4930516bb43
Wes Young <wesley.young@du.edu>
2020-12-08 17:01:06 -0700

    Save button adding to JSON file

--- server.js
+++ server.js
@@ -1,26 +1,47 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
+const { json } = require("express");
 
 const app = express();
 const PORT = 3000;
 
-// app.use(express.static("/assets/css/style.css"));
+//Review these two lines of code.
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
-app.get("/notes.html", (req, res) =>
+app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
-// app.get("/assets/js/index.js", (req, res) => {
-//   app.use(express.static("/assets/js/index.js"));
-// });
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/db/db.json"));
+});
 
-// app.get("/assets/css/styles.css", (req, res) => {
-//   res.type("text/css");
-//   res.end(app.use(express.static("/assets/css/style.css")));
-// });
+app.post("/api/notes", (req, res) => {
+  let newNote = req.body;
+  fs.readFile(path.join(__dirname, "/db/db.json"), (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      storedData = JSON.parse(data);
+      storedData.push(newNote);
+      fs.writeFile(
+        path.join(__dirname, "/db/db.json"),
+        JSON.stringify(storedData),
+        (err) => {
+          if (err) {
+            console.log(err);
+          }
+        }
+      );
+    }
+  });
+});
 
 app.listen(PORT, () => console.log(`App is listening on PORT ${PORT}`));
